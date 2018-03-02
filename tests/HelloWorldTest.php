b1f5a095224218656127ebfe17b5e6907f80aff6
chris-baur <chriskbaur@gmail.com>
2018-03-02 10:31:49 -0500

    commented
    c

--- tests/HelloWorldTest.php
+++ tests/HelloWorldTest.php
@@ -4,13 +4,13 @@ class HelloWorldTest extends PHPUnit_Framework_TestCase
     /**
      * @var PDO
      */
-    private $pdo;
-    public function setUp()
-    {
+    //private $pdo;
+    // public function setUp()
+    // {
         // $this->pdo = new PDO($GLOBALS['db_dsn'], $GLOBALS['db_username'], $GLOBALS['db_password']);
         // $this->pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
         // $this->pdo->query("CREATE TABLE hello (what VARCHAR(50) NOT NULL)");
-    }
+    // }
     // public function tearDown()
     // {
     //     $this->pdo->query("DROP TABLE hello");
