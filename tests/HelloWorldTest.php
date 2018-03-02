6bd7e84b87f3289491eb6869412042f3fa5a40da
chris-baur <chriskbaur@gmail.com>
2018-03-02 11:54:46 -0500

    t11
    t11

--- tests/HelloWorldTest.php
+++ tests/HelloWorldTest.php
@@ -6,26 +6,26 @@ class HelloWorldTest extends PHPUnit\Framework\TestCase
      * @var PDO
      */
     //private $pdo;
-    // public function setUp()
-    // {
-        // $this->pdo = new PDO($GLOBALS['db_dsn'], $GLOBALS['db_username'], $GLOBALS['db_password']);
-        // $this->pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-        // $this->pdo->query("CREATE TABLE hello (what VARCHAR(50) NOT NULL)");
-    // }
-    // public function tearDown()
-    // {
-    //     $this->pdo->query("DROP TABLE hello");
-    // }
+    public function setUp()
+    {
+        $this->pdo = new PDO($GLOBALS['db_dsn'], $GLOBALS['db_username'], $GLOBALS['db_password']);
+        $this->pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        $this->pdo->query("CREATE TABLE hello (what VARCHAR(50) NOT NULL)");
+    }
+    public function tearDown()
+    {
+        $this->pdo->query("DROP TABLE hello");
+    }
     // public function testHelloWorld()
     // {
     //     $helloWorld = new HelloWorld($this->pdo);
     //     $this->assertEquals('Hello World', $helloWorld->hello());
     // }
-    // public function testHello()
-    // {
-    //     $helloWorld = new HelloWorld($this->pdo);
-    //     $this->assertEquals('Hello Bar', $helloWorld->hello('Bar'));
-    // }
+    public function testHello()
+    {
+        $helloWorld = new HelloWorld($this->pdo);
+        $this->assertEquals('Hello Bar', $helloWorld->hello('Bar'));
+    }
     // public function testWhat()
     // {
     //     $helloWorld = new HelloWorld($this->pdo);
