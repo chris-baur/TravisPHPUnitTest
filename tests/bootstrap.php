5efd8200a47650aaed2b3fac244d07b2d754b4de
chris-baur <chriskbaur@gmail.com>
2018-03-06 09:54:19 -0500

    e
    e

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -3,7 +3,7 @@ function loader($class)
 {
     $file = $class . '.php';
     echo 'The class is: '.$class;
-    $file = '..\private\\'.$file;
+    $file = '\private\\'.$file;
     if (file_exists($file)) {
         require $file;
     }
