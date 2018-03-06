a578689f7dc3c8f6324efc6e1d3e04ae5b8a6f78
chris-baur <chriskbaur@gmail.com>
2018-03-06 10:29:05 -0500

    aa
    aa

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -3,6 +3,7 @@ function loader($class)
 {
     $file = $class . '.php';
     echo 'The class is: '.$class;
+    echo 'The directory path is: '. __DIR__;    
     //$file = '\private\\'.$file;
     if (file_exists($file)) {
         require $file;
