fc2f8bb8527849d77fabe2204f150fe8ef216268
chris-baur <chriskbaur@gmail.com>
2018-03-06 10:34:18 -0500

    A
    A

--- tests/HelloWorldTest.php
+++ tests/HelloWorldTest.php
@@ -1,5 +1,5 @@
 <?php
-include_once '../private/HelloWorld.php';
+
 class HelloWorldTest extends PHPUnit\Framework\TestCase
 {
     /**
--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -2,11 +2,8 @@
 function loader($class)
 {
     $file = $class . '.php';
-    echo 'The class is: '.$class;
-    echo 'The directory path is: '. __DIR__;    
-    //$file = '\private\\'.$file;
     if (file_exists($file)) {
-        require $file;
+        require '../private/'.$file;
     }
 }
 spl_autoload_register('loader');
